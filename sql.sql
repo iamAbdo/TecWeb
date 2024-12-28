cba03912831316b5c97e0e17983e65ee7df81ae1
Abdellah <you@example.com>
2024-12-28 15:17:15 +0100

    fixed comments

--- sql.sql
+++ sql.sql
@@ -1,4 +1,4 @@
--- Table to store contact messages from students or visitors
+-- Table to store contact messages
 CREATE TABLE messages (
 id INT AUTO_INCREMENT PRIMARY KEY,
 student_id INT NULL,
@@ -21,7 +21,7 @@ created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 FOREIGN KEY (specialty_id) REFERENCES specialties(id)
 );
 
--- Table to store different specialties offered by the university
+-- Table to store specialties offered by us
 CREATE TABLE specialties (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
@@ -39,7 +39,7 @@ email VARCHAR(100) NOT NULL UNIQUE,
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Table to store modules taught in each specialty (e.g., Symmetric Cryptography)
+-- Table to store modules 
 CREATE TABLE modules (
 id INT AUTO_INCREMENT PRIMARY KEY,
 specialty_id INT NOT NULL,
@@ -47,7 +47,7 @@ name VARCHAR(255) NOT NULL,
 FOREIGN KEY (specialty_id) REFERENCES specialties(id)
 );
 
--- Table to store certificates associated with modules (e.g., Certified Ethical Hacker)
+-- Table to store certificates
 CREATE TABLE certificates (
 id INT AUTO_INCREMENT PRIMARY KEY,
 specialty_id INT NOT NULL,
@@ -57,13 +57,13 @@ image VARCHAR(255) NOT NULL,
 FOREIGN KEY (specialty_id) REFERENCES specialties(id)
 );
 
--- Table to store references (books, articles, certifications) related to modules
+-- Table to store references 
 CREATE TABLE reference (
 id INT AUTO_INCREMENT PRIMARY KEY,
 specialty_id INT NOT NULL,
-name VARCHAR(255) NOT NULL, -- Name of the reference
-author_or_source VARCHAR(255) NOT NULL, -- Author or source of the reference
-description TEXT, -- Description of the reference
-read_more_url VARCHAR(255), -- URL for more information
-FOREIGN KEY (specialty_id) REFERENCES specialties(id) -- Foreign key to modules table
+name VARCHAR(255) NOT NULL,
+author_or_source VARCHAR(255) NOT NULL, 
+description TEXT,
+read_more_url VARCHAR(255), 
+FOREIGN KEY (specialty_id) REFERENCES specialties(id) 
 );
